2e894da0cfc0207df833ad1b45c454dc6b33dba1
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-07-02 18:21:59 +0300

    Add API response format

---
+++ src/main/java/com/pro/fooddonorKE/api/response/ApiResponse.java
@@ -0,0 +1,45 @@
+package com.pro.fooddonorKE.api.response;
+
+import com.google.gson.JsonElement;
+
+public class ApiResponse {
+  private int status;
+  private String message;
+  private JsonElement data;
+
+
+  public ApiResponse(int status, String message) {
+    this.status = status;
+    this.message = message;
+  }
+
+  public ApiResponse(int status, String message, JsonElement data) {
+    this.status = status;
+    this.message = message;
+    this.data = data;
+  }
+
+  public int getStatus() {
+    return status;
+  }
+
+  public void setStatus(int status) {
+    this.status = status;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public JsonElement getData() {
+    return data;
+  }
+
+  public void setData(JsonElement data) {
+    this.data = data;
+  }
+}
