71d36f7b1eadccf759132054294da59fc432c2b1
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-07-03 02:27:21 +0300

    Add get endpoints for charities

--- src/main/java/com/pro/fooddonorKE/api/Main.java
+++ src/main/java/com/pro/fooddonorKE/api/Main.java
@@ -9,19 +9,18 @@ import com.pro.fooddonorKE.api.models.*;
 import com.pro.fooddonorKE.api.response.ApiResponse;
 import org.sql2o.Sql2o;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static java.lang.Integer.parseInt;
 import static spark.Spark.*;
 
 public class Main {
+    static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/fooddonorke", "anna", "pol1234");
+    static Sql2oCharityDao charityDao = new Sql2oCharityDao(sql2o);
+    static Sql2oImageDao imageDao = new Sql2oImageDao(sql2o);
+    static Sql2oFoodDonationDao foodDonationDao = new Sql2oFoodDonationDao(sql2o);
+    static Sql2oCharityContactDao charityContactDao = new Sql2oCharityContactDao(sql2o);
     public static void main(String[] args) {
-        Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/fooddonorke", "anna", "pol1234");
-        Sql2oCharityDao charityDao = new Sql2oCharityDao(sql2o);
-        Sql2oImageDao imageDao = new Sql2oImageDao(sql2o);
-        Sql2oFoodDonationDao foodDonationDao = new Sql2oFoodDonationDao(sql2o);
-        Sql2oCharityContactDao charityContactDao = new Sql2oCharityContactDao(sql2o);
         Sql2oDonationRequestDao donationRequestDao = new Sql2oDonationRequestDao(sql2o);
         Gson gson = new Gson();
 
@@ -149,6 +148,52 @@ public class Main {
             }
         });
 
+        // READ CHARITIES BY LOCATION
+        get("/charities/locations", "application/json", (request, response) -> {
+            List<Charity> charities = charityDao.getCharitiesByLocation(request.queryParams("location"));
+
+            if (charities == null){
+                throw new ApiException("Not a valid argument for 'location'", Response.BAD_REQUEST);
+            } else if(charities.size() == 0){
+                throw new ApiException(String.format("No charities in location '%s' listed", request.queryParams("location")), Response.NOT_FOUND);
+            } else {
+                ApiResponse apiResponse = new ApiResponse(Response.OK.getStatusCode(), "Success", new Gson().toJsonTree(transformCharityList(charities)));
+                return gson.toJson(apiResponse);
+            }
+        });
+
+        // READ CHARITIES BY TYPE
+        get("/charities/types", "application/json", (request, response) -> {
+            List<Charity> charities = charityDao.getCharitiesByType(request.queryParams("type"));
+
+            if (charities == null){
+                throw new ApiException("Not a valid argument for 'type'", Response.BAD_REQUEST);
+            } else if(charities.size() == 0){
+                throw new ApiException(String.format("No charities of type '%s' listed", request.queryParams("type")), Response.NOT_FOUND);
+            } else {
+                ApiResponse apiResponse = new ApiResponse(Response.OK.getStatusCode(), "Success", new Gson().toJsonTree(transformCharityList(charities)));
+                return gson.toJson(apiResponse);
+            }
+        });
+
+        // READ CHARITY
+        get("/charities/:id", "application/json", (request, response) -> {
+            Charity charity = charityDao.get(parseInt(request.params("id")));
+
+            if (charity == null){
+                throw new ApiException(String.format("No charity with id '%s' listed", request.params("id")), Response.NOT_FOUND);
+            } else {
+                ApiResponse apiResponse = new ApiResponse(Response.OK.getStatusCode(), "Success", new Gson().toJsonTree(charity));
+                return gson.toJson(apiResponse);
+            }
+        });
+
+        // READ DONATION REQUESTS BY LOCATION
+
+        // READ DONATION REQUESTS BY CHARITY
+
+        // READ DONATION REQUEST
+
         //FILTERS
         exception(ApiException.class, (exception, request, response) -> {
             ApiResponse apiResponse = new ApiResponse(exception.getStatus().getStatusCode(), exception.getMessage());
@@ -158,5 +203,29 @@ public class Main {
         });
 
         after((request, response) -> response.type("application/json"));
+    }
+
+    public static List<Charity> transformCharityList(List<Charity> charities){
+        ArrayList<Charity> charityArrayList = (ArrayList<Charity>) charities;
+
+        charityArrayList.replaceAll(Main::transformCharity);
+
+        return charityArrayList;
+    }
+
+    public static Charity transformCharity(Charity charity){
+        Image image = imageDao.getImage(charity.getId());
+        List<Image> images = imageDao.getDescriptionImages(charity.getId());
+        List<FoodDonation> foodDonations = foodDonationDao.getFoodDonations(charity.getId());
+        CharityContact contact = charityContactDao.get(charity.getId());
+
+        // Transform charity
+        charity.setImage(image.getUrl());
+        charity.setDescriptionImages(images);
+        charity.setFoodDonationTypes(foodDonations);
+        charity.setContacts(contact);
+
+
+        return charity;
     }
 }
