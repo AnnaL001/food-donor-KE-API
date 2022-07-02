366f1ba12da20d8dd5e4f27f14ba8fa96d56f20a
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-07-02 14:47:16 +0300

    Add parameter resolver for dependency injection

---
+++ src/test/java/com/pro/fooddonorKE/api/dao/parameter_resolver/ImageParameterResolver.java
@@ -0,0 +1,19 @@
+package com.pro.fooddonorKE.api.dao.parameter_resolver;
+
+import com.pro.fooddonorKE.api.models.Image;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+public class ImageParameterResolver implements ParameterResolver {
+  @Override
+  public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+    return parameterContext.getParameter().getType() == Image.class;
+  }
+
+  @Override
+  public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+    return new Image("https://image.png", "primary", 1);
+  }
+}
