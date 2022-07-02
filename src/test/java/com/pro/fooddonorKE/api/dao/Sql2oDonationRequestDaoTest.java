b45ef46f465dddc28fe5fc15eadfed9595f64633
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-07-02 12:36:55 +0300

    Add tests for read functionality

--- src/test/java/com/pro/fooddonorKE/api/dao/Sql2oDonationRequestDaoTest.java
+++ src/test/java/com/pro/fooddonorKE/api/dao/Sql2oDonationRequestDaoTest.java
@@ -1,11 +1,14 @@
 package com.pro.fooddonorKE.api.dao;
 
 import com.pro.fooddonorKE.api.dao.parameter_resolver.DonationRequestParameterResolver;
+import com.pro.fooddonorKE.api.interfaces.DonationRequestDao;
 import com.pro.fooddonorKE.api.models.DonationRequest;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,14 +28,38 @@ class Sql2oDonationRequestDaoTest {
   @DisplayName("Test that a donation request can be added")
   public void add_addsDonationRequest_true(DonationRequest request) {
     donationRequestDao.add(request);
-    assertTrue(donationRequestDao.getAll().contains(request));
+    assertTrue(donationRequestDao.getDonationRequests().contains(request));
   }
 
   @Test
+  @DisplayName("Test that a donation request's id is set upon insert")
   public void add_setsDonationRequestId(DonationRequest request) {
     donationRequestDao.add(request);
     assertNotEquals(0, request.getId());
   }
+
+  @Test
+  @DisplayName("Test that a charity's donation requests can be retrieved")
+  public void getDonationRequests_retrievesRequestsByCharity_true(DonationRequest request) {
+    donationRequestDao.add(request);
+    assertEquals(List.of(request), donationRequestDao.getDonationRequests(request.getCharity_id()));
+  }
+
+  @Test
+  @DisplayName("Test that a donation requests can be retrieved based on location")
+  public void getDonationRequests_retrievesRequestsByLocation_true(DonationRequest request) {
+    donationRequestDao.add(request);
+    assertEquals(List.of(request), donationRequestDao.getDonationRequests(request.getLocation()));
+  }
+
+  @Test
+  @DisplayName("Test that all donation requests can be retrieved")
+  public void getDonationRequests_retrievesAllRequests_true(DonationRequest request) {
+    donationRequestDao.add(request);
+    assertEquals(List.of(request), donationRequestDao.getDonationRequests());
+  }
+
+
 
   @AfterEach
   public void tearDown() {
