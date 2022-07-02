8d193a232a64401b87fcb55785ba330d4ea343fe
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-07-02 11:28:36 +0300

    Update model class

--- src/main/java/com/pro/fooddonorKE/api/models/FoodDonation.java
+++ src/main/java/com/pro/fooddonorKE/api/models/FoodDonation.java
@@ -1,12 +1,17 @@
 package com.pro.fooddonorKE.api.models;
 
+import java.util.Objects;
+
 public class FoodDonation {
     private int id;
     private String name;
     private int charity_id;
 
-    public FoodDonation(int id, String name, int charity_id) {
-        this.id = id;
+    public FoodDonation() {
+    }
+
+    public FoodDonation(String name, int charity_id) {
+        this.id = 0;
         this.name = name;
         this.charity_id = charity_id;
     }
@@ -33,5 +38,18 @@ public class FoodDonation {
 
     public void setCharity_id(int charity_id) {
         this.charity_id = charity_id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FoodDonation that = (FoodDonation) o;
+        return charity_id == that.charity_id && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, charity_id);
     }
 }
